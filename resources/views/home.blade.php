1ea941bad8d6a5342c2a6697c3b0762049e3a1ae
Dwika Ayu Novianti <dwikaayunvnti@gmail.com>
2019-03-08 17:13:39 +0700

    admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">HOME</div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
 
-                <div class="card-body">
+                <div class="panel-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+                        <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                   WELCOME TO KOPIKOE
-
+                    You are logged in!
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,39 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return view('coming-soon');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::match(['get', 'post'], '/admin','AdminController@login');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+Route::group(['middleware' => ['auth']],function(){
+	Route::get('/admin/dashboard','AdminController@dashboard');	
+	Route::get('/admin/settings','AdminController@settings');
+	Route::get('/admin/check-pwd','AdminController@chkPassword');
+	Route::match(['get','post'],'/admin/update-pwd','AdminController@updatePassword');
 
-Route::get('/home', 'HomeController@index')->name('home');
+	// Categories Routes (Admin)
+	Route::match(['get','post'],'/admin/add-category','CategoryController@addCategory');
+	Route::match(['get','post'],'/admin/edit-category/{id}','CategoryController@editCategory');
+	Route::match(['get','post'],'/admin/delete-category/{id}','CategoryController@deleteCategory');
+	Route::get('/admin/view-categories','CategoryController@viewCategories');
+
+	// Products Routes
+	Route::match(['get','post'],'/admin/add-product','ProductsController@addProduct');
+	Route::get('/admin/view-products','ProductsController@viewProducts');
+});
+
+
+Route::get('/logout','AdminController@logout');
+
+
+
