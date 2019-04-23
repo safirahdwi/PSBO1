6b84b83bf09b1179b3f2b212462fd644082f909a
annisamonitha <annisamonitha12@gmail.com>
2019-04-23 23:36:05 +0700

    PSBO1

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,13 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
 use App\Product;
+use App\Category;
 
 class IndexController extends Controller
 {
     public function index(){
+    	//Dalam ascending order secara default
         $productsAll = Product::get();
-        return view('index')->with(compact('productsAll'));
+        //Dalam descending order 
+        $productsAll = Product::orderBy('id','DESC')->get();
+        #Random order
+        $productsAll = Product::inRandomOrder()->get();
+        #Categories and all categories
+        $categories = Category::with('categories')->where(['parent_id'=>0])->get();
+        /*$categories = json_decode(json_encode($categories));
+        echo "<pre>"; print_r($categories); die;*/
+        /*$categories_menu = "";
+        foreach($categories as $cat){
+         	 $categories_menu .= "<div class='panel-heading'>
+									<h4 class='panel-title'>
+										<a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
+											<span class='badge pull-right'><i class='fa fa-plus'></i></span>
+											".$cat->name."
+										</a>
+									</h4>
+								</div>
+								<div id='sportswear' class='panel-collapse collapse'>
+									<div class='panel-body'>
+										<ul>";
+										$sub_categories = Category::where(['parent_id'=>$cat->id])->get();
+							         	foreach($sub_categories as $subcat){
+											$categories_menu .= "<li><a href='".$subcat->url."'>".$subcat->name." </a></li>";
+										}	
+										$categories_menu .= "</ul>
+									</div>
+								</div>
+								";
+         }*/
+        return view('index')->with(compact('productsAll','categories_menu','categories'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.frontLayout.front_design')
 @section('content')
 
-
-
 <section id="slider"><!--slider-->
 		<div class="container">
 			<div class="row">
@@ -76,105 +74,28 @@
 						<h2>Category</h2>
 						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
 							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Sportswear
-										</a>
-									</h4>
-								</div>
-								<div id="sportswear" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Nike </a></li>
-											<li><a href="#">Under Armour </a></li>
-											<li><a href="#">Adidas </a></li>
-											<li><a href="#">Puma</a></li>
-											<li><a href="#">ASICS </a></li>
-										</ul>
+								<?php //echo $categories_menu; ?>
+								@foreach(categories as $cat)
+									<div class="panel-heading">
+											<h4 class='panel-title'>
+												<a data-toggle='collapse' data-parent="#accordian" href="#{{ $cat->id }}">
+													<span class='badge pull-right'><i class='fa fa-plus'></i></span>
+													{{ $cat->name }}
+												</a>
+											</h4>
 									</div>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#mens">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Mens
-										</a>
-									</h4>
-								</div>
-								<div id="mens" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Fendi</a></li>
-											<li><a href="#">Guess</a></li>
-											<li><a href="#">Valentino</a></li>
-											<li><a href="#">Dior</a></li>
-											<li><a href="#">Versace</a></li>
-											<li><a href="#">Armani</a></li>
-											<li><a href="#">Prada</a></li>
-											<li><a href="#">Dolce and Gabbana</a></li>
-											<li><a href="#">Chanel</a></li>
-											<li><a href="#">Gucci</a></li>
-										</ul>
+									<div id='#{{ $cat->id }}' class='panel-collapse collapse'>
+										<div class='panel-body'>
+											<ul>
+												@foreach($cat->categories as $subcat)
+												<li> <a href="#"> {{ $subcat->name }} </a></li>
+												@endforeach
+											</ul>
+										</div>
 									</div>
-								</div>
+								@endforeach
 							</div>
-							
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#womens">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Womens
-										</a>
-									</h4>
-								</div>
-								<div id="womens" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Fendi</a></li>
-											<li><a href="#">Guess</a></li>
-											<li><a href="#">Valentino</a></li>
-											<li><a href="#">Dior</a></li>
-											<li><a href="#">Versace</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Kids</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Fashion</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Households</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Interiors</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Clothing</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Bags</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
+							<div class="panel panel-default"> 
 								<div class="panel-heading">
 									<h4 class="panel-title"><a href="#">Shoes</a></h4>
 								</div>
@@ -219,18 +140,18 @@
 							<div class="product-image-wrapper">
 								<div class="single-products">
 										<div class="productinfo text-center">
-											<img src="{{ asset('images/frontend_images/home/product1.jpg') }}" alt="" />
-											<h2>$56</h2>
-											<p>Easy Polo Black Edition</p>
+											<img src="{{ asset('images/backend_images/products/small/'.$product->image) }}" alt="" />
+											<h2>INR {{ $product->price }}</h2>
+											<p>{{ $product->product_name }}</p>
 											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 										</div>
-										<div class="product-overlay">
+										<!--div class="product-overlay">
 											<div class="overlay-content">
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
+												<h2>INR {{ $product->price }}</h2>
+												<p>{{ $product->product_name }}</p>
 												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 											</div>
-										</div>
+										</div-->
 								</div>
 								<div class="choose">
 									<ul class="nav nav-pills nav-justified">
